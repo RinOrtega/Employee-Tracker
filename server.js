ddb7ba685c1562f19c186ff82ba0bcf08ef6e9b8
Rin Ortega <rinortega01@icloud.com>
2024-07-14 12:46:58 -0500

    added menu navigation

--- server.js
+++ server.js
@@ -48,3 +48,110 @@ function mainPrompt() {
     }) 
 }
 
+async function viewEmployee() {
+    
+    console.log('List of Employees');
+
+    
+    var employee = await db.query(`SELECT * FROM employee;`);
+    employee.rows
+
+    for (let employee of employee.rows) { `
+    ${employee.id}  ${employee.first_name}  ${employee.last_name}    ${employee.role_id}    ${employee.manager_id}`;
+    };
+ 
+   
+    console.log('All Employees Viewed.', employee);
+
+    
+    mainPrompt();
+}
+
+
+async function getDept() {
+    const data = await db.query(`SELECT * FROM department`)
+    console.log('Departments', rows.data);
+
+    viewDepartment(data);
+}
+
+function viewDepartment(data) {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'departments',
+            message: 'What Department would you like to view',
+            
+            
+            
+            choices: [rows.data.name]
+
+            
+        })
+}
+
+function addEmployee() {
+    
+    inquirer
+        .prompt(
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'Enter Employee name',
+        },
+        {
+            type: 'input',
+            name: 'Role',
+            message: "Enter Employee's new Role",
+        },
+    )
+    .then()
+
+    console.log('Successfuly added employee!')
+
+    
+    mainPrompt();
+}
+
+
+function removeEmployee() {
+    console.log('Choose Employee to remove');
+
+    // Return user to main menu
+    mainPrompt();
+}
+
+function updateEmployeeRole() {
+    console.log('Chose employee role to update');
+    // Return user to main menu
+    mainPrompt();
+}
+
+function addRole() {
+    console.log('Enter new role.')
+
+     
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'newRole',
+                message: 'What is the new role?',
+            },
+            {
+                type: 'input',
+                name: 'newRoleSalary',
+                message: 'What is the salary for this new role',
+            },
+        ])
+
+    
+    mainPrompt();
+}
+
+function endApp() {
+    db.end();
+    console.log('Quitting App');
+}
+
+main();
